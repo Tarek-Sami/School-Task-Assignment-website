ac827f323d41e9491e0b304dcc3a6e29a3fc17d9
Roaa Rady <roaarady222@gmail.com>
2026-04-10 19:48:00 +0200

    Update login.js

--- scripts/shared/login.js
+++ scripts/shared/login.js
@@ -2,14 +2,42 @@ const form = document.querySelector("form");
 
 if (form) {
   form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const error = document.getElementById("loginError");
 
-    const role = localStorage.getItem("role");
+    error.innerText = "";
+    error.style.color = "red";
 
-    if (!role) {
-      return (window.location.href = "/shared/signup.html");
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    // Validation
+    if (!email.match(emailPattern)) {
+      e.preventDefault();
+      error.innerText = "Enter a valid email";
+      return;
     }
 
-    window.location.href = `/${role}/dashboard.html`;
+    if (password.length < 6) {
+      e.preventDefault();
+      error.innerText = "Password must be at least 6 characters";
+      return;
+    }
+
+    // if correct
+    error.style.color = "green";
+    error.innerText = "Login successful!";
+
+    e.preventDefault(); //stop temp to show msg
+
+    setTimeout(() => {
+      const role = localStorage.getItem("role");
+
+      if (!role) {
+        window.location.href = "/shared/signup.html";
+      } else {
+        window.location.href = `/${role}/dashboard.html`;
+      }
+    }, 1000);
   });
 }
