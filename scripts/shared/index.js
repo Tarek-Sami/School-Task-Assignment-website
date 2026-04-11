ebca40409d914c81307694ff54de677089bd25d7
Tarek Sami <tareek.sami230@gmail.com>
2026-04-11 19:25:41 +0200

    landing page role based and animation done

--- scripts/shared/index.js
+++ scripts/shared/index.js
@@ -1,6 +1,8 @@
 let role = localStorage.getItem("role");
 const heroButtons = document.querySelector(".hero-btns");
 const ctaButtons = document.querySelector(".cta-btns");
+const hero = document.querySelector(".hero");
+const p = document.querySelector(".words");
 
 if (!role) {
   heroButtons.innerHTML = `
@@ -23,3 +25,30 @@ if (!role) {
     <a href="/shared/about.html" class="btn btn-primary">About Us</a>
   `;
 }
+
+window.onload = () => {
+  hero.classList.add("show");
+  p.classList.add("show");
+  hero.classList.remove("hidden");
+  p.classList.remove("hidden");
+};
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+reveals.forEach((el, index) => {
+  el.style.transitionDelay = `${(index % 5) * 0.1}s`;
+  observer.observe(el);
+});
