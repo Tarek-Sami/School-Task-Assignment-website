f2a9b177346acaa311ccbd142a853358336b74e3
ahmed tolba <atolba388@gmail.com>
2026-04-11 04:44:35 +0200

    added if click on headline in dashboard got to task details style and java

--- scripts/admin/dashboard.js
+++ scripts/admin/dashboard.js
@@ -71,7 +71,7 @@ tasks
     html += `<div class="task" data-id="${task.id}">
           <div class="task-left">
             <div class="task-info">
-<h4 id="task-headline"><a href="task-details.html?id=${task.id}" class="task-headline-link">${task.headline}</a></h4>
+<h4 id="task-headline"><a href="../teacher/task-details.html?id=${task.id}" class="task-headline-link">${task.headline}</a></h4>
               <p id="task-teacher">
                 <img
                   src="https://ui-avatars.com/api/?name=Aris&background=E5E7EB&color=374151&size=24&rounded=true"
